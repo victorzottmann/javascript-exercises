76c29ba825aa752bce5a9c9e6d6bfc3b0c700e5d
Victor Zottmann <zottmann.victor@gmail.com>
2024-05-16 15:52:38 +1000

    complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+  if (times < 0) {
+    return "ERROR";
+  }
+  
+  return str.repeat(times);
 };
 
 // Do not edit below this line
