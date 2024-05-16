a87287b9827f107eda2208eea41c0ece3e0ede49
Victor Zottmann <zottmann.victor@gmail.com>
2024-05-16 20:27:24 +1000

    complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,49 @@
-const findTheOldest = function() {
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]
 
-};
+const calculateAge = function(birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+}
+
+const findTheOldest = (people) => {
+  let oldestPerson = null;
+  let minBirthYear = Infinity;
+
+  for (const person of people) {
+    if (person.yearOfBirth < minBirthYear) {
+      minBirthYear = person.yearOfBirth;
+      oldestPerson = person;
+    }
+  }
+
+  for (const person of people) {
+    let personAge = calculateAge(person.yearOfBirth, person.yearOfDeath);
+    let oldestAge = calculateAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+
+    if (person.yearOfDeath !== undefined && personAge > oldestAge) {
+      oldestPerson = person;
+    }
+  }
+
+  return oldestPerson;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
