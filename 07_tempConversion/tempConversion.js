6b666047e95e5f1c74d3aa2fcf036419e00f95c9
Victor Zottmann <zottmann.victor@gmail.com>
2024-05-16 17:59:43 +1000

    07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const result = (fahrenheit - 32) * 5 / 9;
+  return Math.round(result * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const result = (9 / 5 * celsius) + 32;
+  return Math.round(result * 10) / 10;
 };
 
 // Do not edit below this line
