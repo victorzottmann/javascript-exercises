0bc8a3e08af5028bd44d0c6e5bac2bf65bac40db
Victor Zottmann <zottmann.victor@gmail.com>
2024-05-16 18:46:32 +1000

    complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
+const palindromes = function(s) {
+  const accepted = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+  const str = s.toLowerCase().split("").filter((c) => accepted.includes(c)).join("");
+  const reversed = str.split("").reverse().join("");
+
+  return str === reversed;
 };
 
-// Do not edit below this line
 module.exports = palindromes;
