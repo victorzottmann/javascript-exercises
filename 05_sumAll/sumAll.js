3302d6bd60a967b90ca760195ab7d14e5326fbc8
Victor Zottmann <zottmann.victor@gmail.com>
2024-05-16 17:44:28 +1000

    complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+  if (!Number.isInteger(n1) || !Number.isInteger(n2)) {
+    return "ERROR";
+  }
 
+  if (n1 < 0 || n2 < 0) {
+    return "ERROR";
+  }
+
+  // swap numbers if the first is larger than the second
+  if (n1 > n2) {
+    let temp = n2;
+    n2 = n1;
+    n1 = temp;
+  }
+  
+  let sum = 0;
+  for (let i = n1; i <= n2; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
