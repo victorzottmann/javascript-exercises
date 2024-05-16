97f2766ce511dc853163fd0ae82970c1404a9a64
Victor Zottmann <zottmann.victor@gmail.com>
2024-05-16 18:59:51 +1000

    complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  let num = Number(n);
 
+  if (num < 0) {
+    return "OOPS";
+  }  
+
+  if (num <= 1) {
+    return num;
+  }
+
+  let prev = 0;
+  let current = 1;
+  let next;
+
+  for (let i = 2; i <= num; i++) {
+    next = prev + current;
+    prev = current;
+    current = next
+  }
+
+  return current;
 };
 
 // Do not edit below this line
